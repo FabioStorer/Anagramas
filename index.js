7bc195367a5d13153b970f9e77530ef2a7313901
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-24 19:34:53 -0300

    feat: adicionado a logica do anagrama

--- index.js
+++ index.js
@@ -10,14 +10,13 @@ process.stdin.on('data', function (data) {
     }
 
     console.log(input)
-    
-    for (let i = 1; i < input.length; i++) {
-        for (let j = i + 1; j < input.length; j++) {
-            input = input.split('');
+    input = input.split('');
+    for (let i = 0; i < input.length; i++) {
+        for (let j = 0; j < input.length; j++) {
             let aux = input[i];
             input[i] = input[j];
             input[j] = aux;
         }
-        console.log(input);
+        console.log(input.join(''));
     }
 })
