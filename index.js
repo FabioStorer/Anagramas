3396bac6a27fcb3c0a926a42ed0d7f2bd12b6b79
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-24 19:44:21 -0300

    feat: adicionado gambiarra para sair

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 let input = '';
+let contador = 0;
 
 console.log('Digite uma palavra que irei listar todos os Anagramas existentes da mesma.');
+console.log('Para finalizar, digite "Sair" ou "sair" duas vezes.');
 
 process.stdin.on('data', function (data) {
     input = data.toString().trim();
 
     if (input == 'Sair' || input == 'sair') {
+        contador = contador + 1;
+    }
+
+    if (contador == 2) {
         process.exit();
     }
 
